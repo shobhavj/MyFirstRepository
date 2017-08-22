94b8ab335af57d136ea190008f54ddcbbb70da14
Shobha V J <shobha@Shobhas-Mac-mini.local>
2017-08-21 22:52:23 -0400

    hello world

--- MyFirstProject/ViewController.m
+++ MyFirstProject/ViewController.m
@@ -17,7 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    printf("Hello World");
 }
+
 
 
 - (void)didReceiveMemoryWarning {
